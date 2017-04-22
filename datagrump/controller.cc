bafdea1756a9b3e7641ab6d6091c7e47a4bbf7b2
Melanie Ren <melren@stanford.edu>
2017-04-21 22:02:07 -0700

    Updated threshold size and related window parameters for better power score.

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <math.h>
 
 #include "controller.hh"
 #include "timestamp.hh"
 
 using namespace std;
+
+const unsigned int DEFAULT_WIN = 32;
+const uint64_t DELAY_THRESH = 80;
+const float WAIT_FACTOR = 2;
+unsigned int the_window_size = DEFAULT_WIN;
+uint64_t time_since_last_resize = 0;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
@@ -13,9 +20,6 @@ Controller::Controller( const bool debug )
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
-  /* Default: fixed window size of 100 outstanding datagrams */
-  unsigned int the_window_size = 50;
-
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
 	 << " window size is " << the_window_size << endl;
@@ -48,13 +52,30 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  /* Default: take no action */
+  /* Keep track of time stamp and only decrease window size every period of time instead of every ack received */
+
+  
+  uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
+
+  if (rtt > DELAY_THRESH) {
+    if (timestamp_ms() - time_since_last_resize > WAIT_FACTOR*DELAY_THRESH) {
+      the_window_size = ceil(the_window_size/2);
+      time_since_last_resize = timestamp_ms();
+    }
+  } else {
+    time_since_last_resize = 0;
+    the_window_size += 1;
+    if (the_window_size > DEFAULT_WIN) {
+      the_window_size = DEFAULT_WIN;
+    }
+  }
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
 	 << " (send @ time " << send_timestamp_acked
 	 << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
+	 << "RTT is " << rtt << " time " << time_since_last_resize
 	 << endl;
   }
 }
@@ -63,5 +84,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 1000; /* timeout of one second */
+  return DELAY_THRESH; /* timeout of one second */
 }
